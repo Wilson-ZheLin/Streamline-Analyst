8efb5eb1073238bf38181fd3687b2b40b358baf1
khoitq-acl <khoitq@americancodelab.com>
2024-07-19 20:48:31 +0700

    update prompt

--- app/src/visualization_ai.py
+++ app/src/visualization_ai.py
@@ -18,18 +18,27 @@ Mô tả tổng quan dữ liệu
 '''
 
 column_description = """
-Ngày: Ngày mà thông tin về bất động sản được ghi nhận.
-Địa chỉ: Địa chỉ chi tiết của bất động sản, bao gồm tên đường, phường/xã, quận/huyện.
-Quận: Đơn vị hành chính cấp hành chính lớn hơn phường/xã, nơi bất động sản đặt tại.
-Huyện: Đơn vị hành chính lớn hơn phường/xã nơi bất động sản đặt tại.
-Loại hình nhà ở: Mô tả loại hình của bất động sản, ví dụ nhà ngõ, nhà mặt phố, biệt thự, chung cư, v.v.
-Giấy tờ pháp lý: Trạng thái pháp lý của bất động sản, bao gồm những loại như đã có sổ đỏ, đang chờ cấp sổ, hoặc không rõ ràng.
-Số tầng: Số tầng của bất động sản, bao gồm cả tầng hầm và tầng trệt nếu có.
-Số phòng ngủ: Số lượng phòng ngủ trong bất động sản.
-Diện tích: Diện tích tổng thể của bất động sản, tính bằng mét vuông (m²).
-Dài: Chiều dài của bất động sản, tính bằng mét (m).
-Rộng: Chiều rộng của bất động sản, tính bằng mét (m).
-Giá/m2: Giá bán hoặc giá cho thuê trung bình của một mét vuông bất động sản, tính bằng đơn vị tiền tệ (ví dụ như VND).
+Ngày: Kiểu dữ liệu ngày không phải số, Ngày mà thông tin về bất động sản được ghi nhận.
+Địa chỉ: Kiểu dữ liệu không phải số, Địa chỉ chi tiết của bất động sản, bao gồm tên đường, phường/xã, quận/huyện.
+Quận: Kiểu dữ liệu không phải số, Đơn vị hành chính cấp hành chính lớn hơn phường/xã, nơi bất động sản đặt tại.
+Huyện: Kiểu dữ liệu không phải số, Đơn vị hành chính lớn hơn phường/xã nơi bất động sản đặt tại.
+Loại hình nhà ở: Kiểu dữ liệu không phải số, Mô tả loại hình của bất động sản, ví dụ nhà ngõ, nhà mặt phố, biệt thự, chung cư, v.v.
+Giấy tờ pháp lý: Kiểu dữ liệu không phải số, Trạng thái pháp lý của bất động sản, bao gồm những loại như đã có sổ đỏ, đang chờ cấp sổ, hoặc không rõ ràng.
+Số tầng: Kiểu dữ liệu số, Số tầng của bất động sản, bao gồm cả tầng hầm và tầng trệt nếu có.
+Số phòng ngủ: Kiểu dữ liệu số,  Số lượng phòng ngủ trong bất động sản.
+Diện tích: Kiểu dữ liệu số,  Diện tích tổng thể của bất động sản, tính bằng mét vuông (m²).
+Dài: Kiểu dữ liệu số,  Chiều dài của bất động sản, tính bằng mét (m).
+Rộng: Kiểu dữ liệu số,  Chiều rộng của bất động sản, tính bằng mét (m).
+Giá/m2: Kiểu dữ liệu số,  Giá bán hoặc giá cho thuê trung bình của một mét vuông bất động sản, tính bằng đơn vị tiền tệ (ví dụ như VND).
+"""
+
+chart_description = """
+Scatter plot: Yêu cầu 2 thuộc tính.
+Heatmap: Các thuộc tính không phải số.
+Boxplot: Các thuộc tính không phải số.
+Violin plot: Các thuộc tính không phải số.
+Strip plot: Các thuộc tính không phải số.
+Line plot: Yêu cầu 2 thuộc tính, và các thuộc tính phải là số.
 """
 
 related_template = """
@@ -40,10 +49,10 @@ attrs_template = """  Bạn là một data analyst.  Shape của data frame là 
 
 """
 
-single_attr_chart_visualization_template = """ Bạn là một data analyst.  Shape của data frame là {shape_info}.  Giá trị head(5) của data frame là: {head_info}. Giá trị nunique() của data frame là: {nunique_info}. Mô tả của data frame là: {description_info}. Mô tả các cột là: {column_description}.Các trường cần visusalization là: {attrs}. Câu hỏi của người dùng là: {question}. Data đã được làm sạch và tiền xử lý, các null đã được lấp đầy và mã hoá để sẵn sàng để huấn luyện mô hình máy học. Dựa vào thông tin data cung cấp, hãy giúp tôi quyết định rằng nên dùng biểu đồ nào trên dữ liệu data frame giá nhà được cung cấp. Các biểu đồ tùy chọn là: ['Donut chart', 'Violin plot', 'Distribution histogram', 'Boxplot', 'Density plot', 'Strip plot', 'Distribution boxplot'] Chỉ dữ liệu được trả về ở định dạng json mà không có bất kỳ lời giải thích hoặc nội dung nào khác. Phản hồi mẫu: {{"chart":["Donut chart", "Boxplot"], "reason": ""}}"""
+single_attr_chart_visualization_template = """ Bạn là một data analyst.  Shape của data frame là {shape_info}.  Giá trị head(5) của data frame là: {head_info}. Giá trị nunique() của data frame là: {nunique_info}. Mô tả của data frame là: {description_info}. Mô tả các cột là: {column_description}. Ràng buộc điều kiện các chart là {chart_description}.Các trường cần visusalization là: {attrs}. Câu hỏi của người dùng là: {question}. Data đã được làm sạch và tiền xử lý, các null đã được lấp đầy và mã hoá để sẵn sàng để huấn luyện mô hình máy học. Dựa vào thông tin data cung cấp, hãy giúp tôi quyết định rằng nên dùng biểu đồ nào trên dữ liệu data frame giá nhà được cung cấp. Các biểu đồ tùy chọn là: ['Donut chart', 'Violin plot', 'Distribution histogram', 'Boxplot', 'Density plot', 'Strip plot', 'Distribution boxplot'] Chỉ dữ liệu được trả về ở định dạng json mà không có bất kỳ lời giải thích hoặc nội dung nào khác. Phản hồi mẫu: {{"chart":["Donut chart", "Boxplot"], "reason": ""}}"""
 
-multiple_attr_chart_visualization_template = """ Bạn là một data analyst.  Shape của data frame là {shape_info}.  Giá trị head(5) của data frame là: {head_info}. Giá trị nunique() của data frame là: {nunique_info}. Mô tả của data frame là: {description_info}. Mô tả các cột là: {column_description}.Các trường cần visusalization là: {attrs}. Câu hỏi của người dùng là: {question}. Data đã được làm sạch và tiền xử lý, các null đã được lấp đầy và mã hoá để sẵn sàng để huấn luyện mô hình máy học. Dựa vào thông tin data cung cấp, hãy giúp tôi quyết định rằng nên dùng biểu đồ nào trên dữ liệu data frame giá nhà được cung cấp. Các biểu đồ tùy chọn là: ["Violin plot", "Boxplot", "Heatmap", "Strip plot", "Line plot", "Scatter plot"] Chỉ dữ liệu được trả về ở định dạng json mà không có bất kỳ lời giải thích hoặc nội dung nào khác. Phản hồi mẫu: {{"chart":["Violin plot", "Boxplot"], "reason": ""}}"""
-
+multiple_attr_chart_visualization_template = """ Bạn là một data analyst.  Shape của data frame là {shape_info}.  Giá trị head(5) của data frame là: {head_info}. Giá trị nunique() của data frame là: {nunique_info}. Mô tả của data frame là: {description_info}. Mô tả các cột là: {column_description}. Ràng buộc điều kiện các chart là {chart_description}. Các trường cần visusalization là: {attrs}. Câu hỏi của người dùng là: {question}. Data đã được làm sạch và tiền xử lý, các null đã được lấp đầy và mã hoá để sẵn sàng để huấn luyện mô hình máy học. Dựa vào thông tin data cung cấp, hãy giúp tôi quyết định rằng nên dùng biểu đồ nào trên dữ liệu data frame giá nhà được cung cấp. Các biểu đồ tùy chọn là: [ "Heatmap", "Strip plot", "Line plot", "Scatter plot"] Chỉ dữ liệu được trả về ở định dạng json mà không có bất kỳ lời giải thích hoặc nội dung nào khác. Phản hồi mẫu: {{"chart":["Violin plot", "Boxplot"], "reason": ""}}"""
+#"Violin plot", "Boxplot",
 
 def create_param(df, model, api_key, question):
     
@@ -111,9 +120,9 @@ def predict_visualization_chart(param, template, attrs_value):
     description_info = param["description_info"]
     question = param["question"]
     prompt_template = PromptTemplate(input_variables=[
-        "shape_info", "head_info", "nunique_info", "description_info", "question", "column_description", "attrs"], template=template)
+        "shape_info", "head_info", "nunique_info", "description_info", "question", "column_description","chart_description", "attrs"], template=template)
     summary_prompt = prompt_template.format(shape_info=shape_info, head_info=head_info, nunique_info=nunique_info,
-                                            description_info=description_info, column_description=column_description, attrs=attrs_value["attrs"], question=question)
+                                            description_info=description_info, column_description=column_description,chart_description=chart_description, attrs=attrs_value["attrs"], question=question)
 
     value = question_llm(param, summary_prompt)
     return value
